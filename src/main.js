97bfe1cfd5ab37ebb9da0473230d16f7e02ee5a1
ColetteOrdz <pcoletteor@gmail.com>
2021-07-18 21:26:33 -0500

    Mostrar títulos

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
+//import ghibli from './data/ghibli/ghibli.json';
 import  data  from './data/ghibli/ghibli.js';
 
+const showTitle = document.getElementById("titulos");
+const ghibliFilms = data.films;
+
+var titulos = ghibliFilms.map( titulo => {
+    return titulo.title;
+})
+
+showTitle.innerHTML = titulos;
+
+console.log(titulos);
 
 
 
-
-//console.log(films.films[0].title);
