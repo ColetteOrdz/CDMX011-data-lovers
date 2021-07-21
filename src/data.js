d35b04c652471207b98f5d1a2519fda137e558c4
ColetteOrdz <pcoletteor@gmail.com>
2021-07-21 00:06:08 -0500

    Funcion sort y mostrarlo en la web

--- src/data.js
+++ src/data.js
@@ -1,10 +1,28 @@
 import  data  from './data/ghibli/ghibli.js'; 
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
+//export const anotherExample = () => {
+//  return 'OMG';
+//};
+
+const ghibliFilms = data.films; //constante donde se almacena toda la información de la BD
+
+//Muestra los array de las películas de acuerdo a su puntuación
+export const filterByScore = () => {
+  const filterHighScore = ghibliFilms.filter(film => film.rt_score >= 90);
+  const filterMediumScore = ghibliFilms.filter(film => film.rt_score >= 80 && film.rt_score < 90);
+  const filterLowScore = ghibliFilms.filter(film => film.rt_score < 80);    
+  //console.log(filterHighScore);
+
+    return filterByScore;
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+//Ordena los array por score más alto al más bajo
+export const sortFilms = (categoria) => {
+  console.log(typeof categoria)
+  const sortScore = ghibliFilms.sort((scr1, scr2) => {
+    return scr2[categoria] - scr1[categoria]
+  });
+  console.log(sortScore);
+    return sortScore;
 };
+//console.log(scoreFilms);
--- src/main.js
+++ src/main.js
@@ -1,32 +1,46 @@
 import  data  from './data/ghibli/ghibli.js'; 
+import { sortFilms } from './data.js'; 
 
 const showTitle = document.getElementById("titulos"); 
-const ghibliFilms = data.films; //constante donde se almacena toda la información de la BD
+//var infoFilms = (films) => {
+  //  return films.map( titulo => { 
+  //  return `
+  //  <div class="card">
+  //  <img src="${titulo.poster}" alt=${titulo.title}>
+  //  <h4>${titulo.title}</h4>
+  //  <p>${titulo.release_date}</p>
+  //  <p>Score ${titulo.rt_score}%</p>
+  //  </div>
+  //  `;
+//})}; 
+
+let ghibliFilms = data.films; //constante donde se almacena toda la información de la BD
+//showTitle.innerHTML = infoFilms(ghibliFilms);
+//infoFilms(ghibliFilms); //Le tengo que hablar a la función para darle el parámetro
+
+let activeOpt = document.querySelector("#selectorOrden"); //Llama al botón de select por ID
+activeOpt.addEventListener("click", ()=>{
+    let showSort = document.querySelector(".sortSelector");
+    let currentOpt = showSort.options[showSort.selectedIndex].value; //Captura/trae el valor de la option
+    console.log(currentOpt);
+    sortFilms(currentOpt) //Se está invocando a la función. Para que la función trabaje, necesita su respectivo valor del selector
+    let sortData = sortFilms(currentOpt) //Se está guardando el valor de retorno de la función
+    ghibliFilms = sortData;
+    var infoFilms = ghibliFilms.map( titulo => { 
+        return `
+        <div class="card">
+        <img src="${titulo.poster}" alt=${titulo.title}>
+        <h4>${titulo.title}</h4>
+        <p>${titulo.release_date}</p>
+        <p>Score ${titulo.rt_score}%</p>
+        </div>
+        `;
+    })
+    showTitle.innerHTML = infoFilms;
+});
+ 
 
 // Const donde se aplica el método .map() para imprimir la información con una función, en las tarjetas
-const infoFilms = ghibliFilms.map( titulo => { 
-    return `
-    <div class="card">
-          <img src="${titulo.poster}" alt=${titulo.title}>
-          <h4>${titulo.title}</h4>
-          <p>${titulo.release_date}</p>
-          <p>Score ${titulo.rt_score}%</p>
-        </div>
-    `;
-})
-showTitle.innerHTML = infoFilms;
-
-//Muestro los arrays de las películas de acuerdo a su puntuación
-const filterHighScore = ghibliFilms.filter(film => film.rt_score >= 90);
-const filterMediumScore = ghibliFilms.filter(film => film.rt_score >= 80 && film.rt_score < 90);
-const filterLowScore = ghibliFilms.filter(film => film.rt_score < 80);  
-   // console.log(filterHighScore);
-    
-
-
-
-
-
 
 
 
