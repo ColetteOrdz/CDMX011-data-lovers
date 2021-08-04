c49fd87b4de0989f298713cde5bd6b59dbb4b4e9
rossvel-hub <80022983+rossvel-hub@users.noreply.github.com>
2021-08-04 16:28:09 -0500

    ultimos cambios css/html/js/readme

--- src/main.js
+++ src/main.js
@@ -1,89 +1,103 @@
-import  data  from './data/ghibli/ghibli.js'; 
+import dataFunciones from './data.js';
 
-const showTitle = document.getElementById("titulos"); 
-const ghibliFilms = data.films; //constante donde se almacena toda la información de la BD
+const divContenedorTarjetasPeliculas = document.getElementById("contenedorTarjetasPeliculas");
 
-// Const donde se aplica el método .map() para imprimir la información con una función, en las tarjetas
-const infoFilms = ghibliFilms.map( titulo => { 
-    return `
-    <div class="card">
-          <img src="${titulo.poster}" alt=${titulo.title}>
-          <h4>${titulo.title}</h4>
-          <p>${titulo.release_date}</p>
-          <p>Score ${titulo.rt_score}%</p>
-          <p><a href="otra_pagina.html"> Ver mas</a></p>
-        </div>
-    `;
+//se invoca la funcion de mostrar todo para cargar las tarjetas
+const dataPeliculas = dataFunciones.mostrarTodo()
+divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataPeliculas);
+
+//El evento onchange del select Año, filtrara cuando se selectione una opcion del select
+document.getElementById("selectYear").addEventListener("change", function (e) {
+    //el parametro "e" hace realacion al evento y en el evento nos da que elemento ejecuto el cambio
+    let year = e.currentTarget.value;
+    const peliculasFiltradaAño = dataFunciones.filtrarPorAño(year);
+    //el innerHTML lee el strig y lo interpreta e inyecta como html
+    divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(peliculasFiltradaAño);
+    //le asignamos los titulos descriptivos en le select
+    document.getElementById("selectScore").value = "";
+    document.getElementById("selectDirector").value = "";
+    document.getElementById("selectOrdenarTitulo").value = "";
+    document.getElementById("selectOrdenarYear").value = "";
 })
-showTitle.innerHTML = infoFilms;
-
-//Muestro los arrays de las películas de acuerdo a su puntuación
-const filterHighScore = ghibliFilms.filter(film => film.rt_score == 89);
-const filterMediumScore = ghibliFilms.filter(film => film.rt_score >= 80 && film.rt_score < 90);
-const filterLowScore = ghibliFilms.filter(film => film.rt_score < 80);  
-    console.log(filterHighScore);
-
 
 //Evento onchange del select Año osea que se filtrara cuando se selectione una opcion
 //del select
-document.getElementById("selectYear").addEventListener("change", function(){
-
-    const tarjetasFiltradas = ghibliFilms.filter(film => film.release_date == this.value);
-
-    const cards = tarjetasFiltradas.map( titulo => { 
-        return `
-        <div class="card">
-              <img src="${titulo.poster}" alt=${titulo.title}>
-              <h4>${titulo.title}</h4>
-              <p>${titulo.release_date}</p>
-              <p>Score ${titulo.rt_score}%</p>
-              <p><a href="otra_pagina.html"> Ver mas</a></p>
-            </div>
-        `;
-    })
-    showTitle.innerHTML = cards;
-})  
+document.getElementById("selectScore").addEventListener("change", function (e) {
+    let score = e.currentTarget.value;
+    divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataFunciones.filtrarPorScore(score));
+    document.getElementById("selectYear").value = "";
+    document.getElementById("selectDirector").value = "";
+    document.getElementById("selectOrdenarTitulo").value = "";
+    document.getElementById("selectOrdenarYear").value = "";
+})
 
 //Evento onchange del select Año osea que se filtrara cuando se selectione una opcion
 //del select
-document.getElementById("selectScore").addEventListener("change", function(){
+document.getElementById("selectDirector").addEventListener("change", function (e) {
+    let director = e.currentTarget.value;
+    divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataFunciones.filtrarPorDirector(director));
+    document.getElementById("selectScore").value = "";
+    document.getElementById("selectYear").value = "";
+    document.getElementById("selectOrdenarTitulo").value = "";
+    document.getElementById("selectOrdenarYear").value = "";
+});
 
-    const tarjetasFiltradas = ghibliFilms.filter(film => film.rt_score == this.value);
+//Ordena tarjeta por titulo a-z/z-a
+document.getElementById("selectOrdenarTitulo").addEventListener("change", function (e) {
+    const orden = e.currentTarget.value;
+    divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataFunciones.ordenarPorTitulo(orden));
+    
+    //Esta es la manera larga de escribir la linea 47
+    //const dataPeliculas = dataFunciones.ordenarPorTitulo(orden);
+    //divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataPeliculas);
 
-    const cards = tarjetasFiltradas.map( titulo => { 
-        return `
-        <div class="card">
-              <img src="${titulo.poster}" alt=${titulo.title}>
-              <h4>${titulo.title}</h4>
-              <p>${titulo.release_date}</p>
-              <p>Score ${titulo.rt_score}%</p>
-              <p><a href="otra_pagina.html"> Ver mas</a></p>
-            </div>
-        `;
-    })
-    showTitle.innerHTML = cards;
-})   
+});
 
-//Evento onchange del select Año osea que se filtrara cuando se selectione una opcion
-//del select
-document.getElementById("selectDirector").addEventListener("change", function(){
+//Orden asendente y descendente por año
+document.getElementById("selectOrdenarYear").addEventListener("change", function (e) {
+    const orden = e.currentTarget.value;
+    divContenedorTarjetasPeliculas.innerHTML = construirHTMLDeTarjetas(dataFunciones.ordenarPorAño(orden));
+});
 
-    const tarjetasFiltradas = ghibliFilms.filter(film => film.director == this.value);
+//Cerrar modal
+document.getElementById("modal-cerrar").addEventListener("click", function(){
+    //tomamos el div de modal, mandado a llamar por la clase y le aplica los estilos.
+    let modal = document.querySelectorAll(".modal")[0];
+    modal.style.opacity = "0";
+    modal.style.visibility = "hidden";
+});
 
-    const cards = tarjetasFiltradas.map( titulo => { 
-        return `
-        <div class="card">
-              <img src="${titulo.poster}" alt=${titulo.title}>
-              <h4>${titulo.title}</h4>
-              <p>${titulo.release_date}</p>
-              <p>Score ${titulo.rt_score}%</p>
-              <p><a href="otra_pagina.html"> Ver mas</a></p>
-            </div>
-        `;
-    })
-    showTitle.innerHTML = cards;
-})   
-//console.log(titleYear);
+//Abrir modal cuando se da click a cualquier parte del documento pero siempre y cuando se valide si
+//se le dio un click al boton de ver mas de las cartas esta validacion esta en la linea 64
+document.addEventListener("click", function(e){
+    if (e.target.getAttribute("id") == "btn-modal") {
+        let modal = document.querySelectorAll(".modal")[0];
+        modal.style.opacity = "1";
+        modal.style.visibility = "visible";
+        let idPelicula = e.target.getAttribute("data-id");
+        let data = dataFunciones.buscarPeliculaPorId(idPelicula);
+        document.getElementById("modal-titulo").innerText=data[0].title;
+        document.getElementById("modal-descripcion").innerText=data[0].description;
 
+    }
+});
 
+//Construir el HTML de las cartas, usa como parametro la data
+function construirHTMLDeTarjetas(baseDatos) {
+    let divContenedorTarjetasPeliculas = "";
+    //iteramos cada pelicula para construir el string HTML
+
+    baseDatos.map(pelicula => {
+      divContenedorTarjetasPeliculas += `
+      <div class="card">
+            <img src="${pelicula.poster}" alt=${pelicula.title}>
+            <h4>${pelicula.title}</h4>
+            <p>${pelicula.release_date}</p>
+            <p>Score ${pelicula.rt_score}%</p>
+            <button id="btn-modal" data-id="${pelicula.id}">Ver mas</button>  
+          </div>
+      `;
+    });
+    return divContenedorTarjetasPeliculas;
+  }
 
